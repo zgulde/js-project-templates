8416d6400e6284cc7fe427f6a8d4255d10fac307
Zach Gulde <zachgulde@gmail.com>
2017-08-13 11:09:49 -0500

    first

---
+++ readme.markdown
@@ -0,0 +1,3 @@
+# Node Project Templates
+
+This repo holds some npm project templates that I like to use.
---
+++ templates/lumo/my_project/core.cljs
@@ -0,0 +1,7 @@
+(ns my-project.core
+  (:require [my-project.util :as util]))
+
+(defn -main []
+  (println (util/red "Hello,") (util/blue "World") (util/green "!")))
+
+
