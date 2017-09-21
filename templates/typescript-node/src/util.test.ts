98bb7b6308786f53e8eae0c8d8c0dc2ea9f2dc8d
Zach Gulde <zachgulde@gmail.com>
2017-09-20 21:56:16 -0500

    Add utils for typescript template

--- templates/typescript-node/src/util.test.ts
+++ templates/typescript-node/src/util.test.ts
@@ -1,7 +1,14 @@
-import {sayHello} from './util'
+import {sayHello, now} from './util'
 
 describe('sayHello', () => {
   it('says hello', () => {
     expect(sayHello()).toBe('Hello, World!')
+  })
+})
+
+describe('now', () => {
+  let dateRe = /\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}/
+  it('gets the current timestamp', () => {
+    expect(now()).toMatch(dateRe)
   })
 })
--- templates/typescript-node/src/util.ts
+++ templates/typescript-node/src/util.ts
@@ -1,2 +1,5 @@
 
 export const sayHello = (name = 'World'): string => `Hello, ${name}!`
+
+export const now = () =>
+  new Date().toISOString().slice(0, 19).replace('T', ' ')
